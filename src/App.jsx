84f9571f983e1a35fa9c04d28e88319f82ae3d54
suliman hakimi <afgsuliman50@gmail.com>
2025-07-06 11:57:43 +0430

    Replace home page placeholder with FacebookLogin component

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import GoogleLogin from './pages/google';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<div>Home Page</div>} />
+      <Route path="/" element={<FacebookLogin />} />
       <Route path="/facebook" element={<FacebookLogin />} />
       <Route path="/google" element={<GoogleLogin />} />
     </Routes>
